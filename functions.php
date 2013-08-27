c3fed38b0aafeb74bdd24fce4ced5c7af2f534aa
Miguel Peixe <miguel@cardume.art.br>
2013-08-27 18:32:48 -0300

    translate all taxonomies

--- functions.php
+++ functions.php
@@ -161,3 +161,24 @@ function toolkit_category_list() {
 	<?php
 	}
 }
+
+function toolkit_qtranslate_edit_taxonomies() {
+	$args = array(
+		'public' => true ,
+		'_builtin' => false
+	);
+
+	$output = 'object'; // or objects
+	$operator = 'and'; // 'and' or 'or'
+
+	$taxonomies = get_taxonomies($args,$output,$operator); 
+
+	if($taxonomies) {
+		foreach ($taxonomies  as $taxonomy ) {
+			add_action($taxonomy->name.'_add_form', 'qtrans_modifyTermFormFor');
+			add_action($taxonomy->name.'_edit_form', 'qtrans_modifyTermFormFor');
+		}
+	}
+
+}
+add_action('admin_init', 'toolkit_qtranslate_edit_taxonomies');
