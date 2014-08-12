d9013b0869b5c85b2e68ab25bbefc3ef4c4be67d
Miguel Peixe <miguel@cardume.art.br>
2014-08-12 17:58:59 -0300

    more design on section tracks

--- section-tracks.php
+++ section-tracks.php
@@ -1,17 +1,59 @@
 <section id="tracks" class="row anti-row">
-
 	<div class="container">
-		<div class="twelve columns row">
-			<h2><?php _e('Tutorial tracks'); ?></h2>
-		</div>
 		<div class="row">
 			<div class="four columns">
+				<h2 class="htitle"><?php _e('Tutorial tracks'); ?></h2>
 				<p class="tracks-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque et blandit quam, in dignissim justo. Morbi id egestas urna. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Donec hendrerit purus eu convallis luctus. Pellentesque non ipsum augue.</p>
 			</div>
 			<div class="four columns">
-			
+				<h3 class="htitle"><?php _e('Featured tracks'); ?></h3>
+				<ul class="track-list">
+					<li class="active">
+						<article>
+							<h3>Lorem</h3>
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer in bibendum velit, vel malesuada ipsum. Praesent nulla enim, dictum eu tempus sit amet, viverra et ipsum.</p>
+						</article>
+					</li>
+					<li>
+						<article>
+							<h3>Ipsum</h3>
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer in bibendum velit, vel malesuada ipsum. Praesent nulla enim, dictum eu tempus sit amet, viverra et ipsum.</p>
+						</article>
+					</li>
+					<li>
+						<article>
+							<h3>Ipsum</h3>
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer in bibendum velit, vel malesuada ipsum. Praesent nulla enim, dictum eu tempus sit amet, viverra et ipsum.</p>
+						</article>
+					</li>
+					<li>
+						<article>
+							<h3>Ipsum</h3>
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer in bibendum velit, vel malesuada ipsum. Praesent nulla enim, dictum eu tempus sit amet, viverra et ipsum.</p>
+						</article>
+					</li>
+					<li>
+						<article>
+							<h3>Ipsum</h3>
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer in bibendum velit, vel malesuada ipsum. Praesent nulla enim, dictum eu tempus sit amet, viverra et ipsum.</p>
+						</article>
+					</li>
+					<li>
+						<article>
+							<h3>Ipsum</h3>
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer in bibendum velit, vel malesuada ipsum. Praesent nulla enim, dictum eu tempus sit amet, viverra et ipsum.</p>
+						</article>
+					</li>
+				</ul>
+			</div>
+			<div class="four columns">
+				<div class="no-title"></div>
+				<div class="single-track">
+					<h2>Lorem</h2>
+					<p class="track-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque et blandit quam, in dignissim justo. Morbi id egestas urna... <a href="#"><?php _e('Continue reading', 'toolkit'); ?></a></p>
+					<p class="track-tutorials"><?php printf(__('This track has <strong>%s</strong> tutorials', 'toolkit'), 2); ?></p>
+				</div>
 			</div>
 		</div>
 	</div>
-
 </section>
