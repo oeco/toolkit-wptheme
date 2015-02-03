6c75198a3a9e0f2bc1cf240328dbbef5b14880f8
Miguel Peixe <miguel@cardume.art.br>
2015-02-03 14:25:37 -0200

    fix texts and add pt-br translation

--- section-site-description.php
+++ section-site-description.php
@@ -1,9 +1,9 @@
 <section id="site-description" class="row anti-row">
 	<div class="container">
 		<div class="twelve columns">
-			<p class="main-text"><?php _e('Geojournalism.org provides online resources and training for <a href="#">journalists</a>, <a href="#">designers</a> and <a href="#">developers</a> to dive into the world of data visualization using geographic data.', 'toolkit'); ?></p>
+			<p class="main-text"><?php printf(__('Geojournalism.org provides online resources and training for <a href="%s">journalists</a>, <a href="%s">designers</a> and <a href="%s">developers</a> to dive into the world of data visualization using geographic data.', 'toolkit'), '#', '#', '#'); ?></p>
 			<div id="live-search">
-				<input type="text" placeholder="Search for tutorials, case studies and concepts..." />
+				<input type="text" placeholder="<?php _e('Search for tutorials, case studies and concepts...', 'toolkit'); ?>" />
 			</div>
 		</div>
 	</div>
