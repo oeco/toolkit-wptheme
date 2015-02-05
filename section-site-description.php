caac2d945c7d278c11fbc412ad0667900c8db882
Miguel Peixe <miguel@cardume.art.br>
2015-02-05 14:04:55 -0200

    add site description anchored links, adjust author image size and create anchor element css

--- section-site-description.php
+++ section-site-description.php
@@ -1,7 +1,11 @@
 <section id="site-description" class="row anti-row">
 	<div class="container">
 		<div class="twelve columns">
-			<p class="main-text"><?php printf(__('Geojournalism.org provides online resources and training for <a href="%s">journalists</a>, <a href="%s">designers</a> and <a href="%s">developers</a> to dive into the world of data visualization using geographic data.', 'toolkit'), '#', '#', '#'); ?></p>
+			<?php
+			$about_page = get_page_by_path('about');
+			$url = get_permalink($about_page->ID);
+			?>
+			<p class="main-text"><?php printf(__('Geojournalism.org provides online resources and training for <a href="%s">journalists</a>, <a href="%s">designers</a> and <a href="%s">developers</a> to dive into the world of data visualization using geographic data.', 'toolkit'), $url . '#for-journalists', $url . '#for-designers', $url . '#for-developers'); ?></p>
 			<div id="live-search">
 				<input type="text" placeholder="<?php _e('Search for tutorials, case studies and concepts...', 'toolkit'); ?>" />
 			</div>
